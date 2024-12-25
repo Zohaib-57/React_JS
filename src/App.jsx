fd109c45c068c7b700e23f89b0af4a158f51cf1c
Your Name <zohaibabbas8557@gmail.com>
2024-12-25 20:59:40 +0500

    worked on import/export

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
-
+import Title from './Title'
+// import { Discription } from './Title'
 function App() {
 
   return (
     <>
     <h1>Hello World</h1>
-    <button>Hello</button>
+    <button>Hello,There:</button>
+    <Title />  {/*rendered the title component*/}
+    <Discription />
     </>
   )
 }
---
+++ src/Title.jsx
@@ -0,0 +1,18 @@
+
+// Working on Components
+
+ function Title(){
+        return <h1>I am Title Component</h1>
+  }
+// export fucntion Discription(){}      ---->Named Export..
+
+  function Discription(){
+    return (
+    <div>
+      <h1>I am Discription Component</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ex repudiandae eveniet consequatur, cumque sequi mollitia atque obcaecati error distinctio. Similique deleniti cumque doloremque pariatur esse! Quod expedita laudantium obcaecati incidunt neque culpa minus praesentium ratione pariatur officiis perspiciatis, aliquid reiciendis eaque! Quod, quia nulla.</p>
+      </div>
+    );
+  }
+export default Title; //default export
+// export {Title, Discription} _______ named export used to export multiple components
