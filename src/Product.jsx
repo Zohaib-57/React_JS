366d67ba1cec1d8273fc160969c528fc13368b51
Your Name <zohaibabbas8557@gmail.com>
2024-12-26 11:23:14 +0500

    done with props

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import './Product.css'
 
-const Product = () => {
+// const Product = () => {
+//   return (
+//     <div className='product'>
+//         <h3>Product Title</h3>
+//         <h5>Product Discription</h5>
+//     </div>
+//   )
+// }
+const Product = ({ title, price, imgUrl ,feature}) => {
   return (
-    <div className='product'>
-        <h3>Product Title</h3>
-        <h5>Product Discription</h5>
-    </div>
-  )
-}
+      <div className='product'>
+          <h3>{title}</h3>
+          <h5>{price}</h5> {/* Working with props*/}
+    <img src={imgUrl} alt={title} width={180} height={180}/>
+    <p>{feature}</p>
+      </div>
+  );
+};
 
 export default Product;
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,14 +2,35 @@ import React from 'react'
 import Product from './Product'
 import './ProductList.css'
 
+// const ProductList = () => {
+//   return (
+//     <div className='product-list'>
+//     <Product />
+//     <Product />
+//     <Product />
+//     </div>
+//   )
+// }
 const ProductList = () => {
+  let Phonefeatures = ["4GB RAM", "64GB Storage", "48MP Camera", "5000mAh Battery"];  // Passing the array to the  props
+  let LaptopFeature ={ a:"16GB RAM", b:"1TB SSD", c:"4K Display", d:"Intel Core i9"}; // passing the object to the props
+  let WatchFeature = ["Heart Rate Monitor", "Sleep Tracker", "Waterproof", "Bluetooth"];
   return (
     <div className='product-list'>
-    <Product />
-    <Product />
-    <Product />
+  <Product title={"Redmi Note-12"} price={"Rs 30000/-"}
+    imgUrl="https://static-01.daraz.pk/p/a8261ad7669e4c2fe21db03de3e66088.jpg"
+    feature={Phonefeatures}/>
+    
+  <Product title="HP Spectre x360" price="Rs 326000/-" 
+   imgUrl="https://starcity.pk/wp-content/uploads/2023/05/HP-Spectre-x360-Convertible-13-AC0xx-4.jpg"
+   feature={LaptopFeature}/>
+   
+
+  <Product title={"Smart Watch-i Series"} price={"Rs 4500/-"}
+    imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRygBL_dNjqtBnCSEtlDjnQHfVWUzUe37VlKg&s" 
+    feature={WatchFeature}/>
     </div>
   )
 }
 
-export default ProductList
+export default ProductList;
